97fbbe416d8ac754e4e25d6e2bb246e62f5b931b
Hannes Junnila <hannes.junnila@gmail.com>
2015-02-18 16:10:31 +0200

    Remove dependency to GTFS-files

--- siri2gtfsrt.py
+++ siri2gtfsrt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import transitfeed
 import json
 import gtfs_realtime_pb2
 from google.protobuf import text_format
@@ -11,10 +10,7 @@ import os
 
 url = "http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL"
 
-feed = transitfeed.Loader(feed_path="HSL.zip", load_stop_times=False, problems=transitfeed.ProblemReporter(None)).Load()
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -27,42 +23,46 @@ def index():
     for i, vehicle in enumerate(siri_data['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']):
         route_id = vehicle['MonitoredVehicleJourney']['LineRef']['value'][:5].strip()
 
-        if route_id in ('1300', '1300V', '1300M'):
+        if route_id in ('1300', '1300V', '1300M' ):
             continue # No other information than location for metros
 
-        if 'Delay' not in vehicle['MonitoredVehicleJourney']:
-            app.logger.debug("No delay for: " + str(vehicle))
-            continue
+	if route_id[0] in ('k', 'K'):
+            continue # Kutsuplus
 
-        if route_id not in feed.routes:
-            app.logger.debug("No route for: " + str(vehicle))
+        if 'Delay' not in vehicle['MonitoredVehicleJourney']:
+            print(vehicle)
             continue
 
         ent = msg.entity.add()
         ent.id = str(i)
+        ent.trip_update.timestamp = vehicle['RecordedAtTime']/1000
         ent.trip_update.trip.route_id = route_id
 
-        route = feed.GetRoute(route_id)
+        ent.trip_update.trip.start_date = vehicle['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DataFrameRef']['value'].replace("-","")
+        if 'DatedVehicleJourneyRef' in vehicle['MonitoredVehicleJourney']['FramedVehicleJourneyRef']:
+            time = vehicle['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
+            ent.trip_update.trip.start_time = time[:2]+":"+time[2:]+":00"
+        
+        if 'DirectionRef' in vehicle['MonitoredVehicleJourney'] and 'value' in vehicle['MonitoredVehicleJourney']['DirectionRef']:
+            ent.trip_update.trip.direction_id = int(vehicle['MonitoredVehicleJourney']['DirectionRef']['value'])-1
+        else:
+            print(vehicle)
 
-        for trip in route.trips:
-            date = vehicle['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DataFrameRef']['value'].replace("-","")
-            if not trip.service_period.IsActiveOn(date):
-                continue
-            start_time = vehicle['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
-            if trip.trip_id[-4:] != start_time:
-                continue
-            if trip.trip_id[-6:-5] != vehicle['MonitoredVehicleJourney']['DirectionRef']['value']:
-                app.logger.debug("invalid direction for " + vehicle['MonitoredVehicleJourney']['DirectionRef']['value'])
-                continue
-
-            ent.trip_update.trip.trip_id = trip.trip_id
-
-        if not ent.trip_update.trip.trip_id:
-            app.logger.debug("No trip for: " + str(vehicle))
+        if 'VehicleRef' in vehicle['MonitoredVehicleJourney']:
+            ent.trip_update.vehicle.label = vehicle['MonitoredVehicleJourney']['VehicleRef']['value']
+        else:
+            print(vehicle)
 
         stoptime = ent.trip_update.stop_time_update.add()
-        stoptime.stop_sequence = 1  # TODO: Get real stop index
-        stoptime.arrival.delay = vehicle['MonitoredVehicleJourney']['Delay']
+
+        if 'MonitoredCall' in vehicle['MonitoredVehicleJourney']:
+            if 'StopPointRef' in vehicle['MonitoredVehicleJourney']['MonitoredCall']:
+                stoptime.stop_id = vehicle['MonitoredVehicleJourney']['MonitoredCall']['StopPointRef']
+            elif 'Order' in vehicle['MonitoredVehicleJourney']['MonitoredCall']:
+                stoptime.stop_sequence = vehicle['MonitoredVehicleJourney']['MonitoredCall']['Order']
+            stoptime.arrival.delay = vehicle['MonitoredVehicleJourney']['Delay']
+        else:
+            ent.trip_update.delay = vehicle['MonitoredVehicleJourney']['Delay']
 
     if 'debug' in request.args:
         return text_format.MessageToString(msg)
@@ -73,4 +73,4 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.debug = True
-    app.run(host='0.0.0.0', port=port, use_reloader=False)
+    app.run(host='0.0.0.0', port=port)
