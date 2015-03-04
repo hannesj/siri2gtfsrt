4d1e4ec6e85d0095a2a0785777772aabe945e04a
Hannes Junnila <hannes.junnila@gmail.com>
2015-03-04 11:24:41 +0200

    Refactor out transitfeed

--- journeys2gtfsrt.py
+++ journeys2gtfsrt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import transitfeed
 import json
 import gtfs_realtime_pb2
 from google.protobuf import text_format
@@ -14,8 +13,6 @@ import pytz
 import os
 
 url = "http://data.itsfactory.fi/journeys/api/1/vehicle-activity"
-
-feed = transitfeed.Loader(feed_path="tampere_gtfs_latest.zip", problems=transitfeed.ProblemReporter(None)).Load()
 
 EPOCH = datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)
 
@@ -33,43 +30,28 @@ def index():
     msg.header.timestamp = int(time.time())
 
     for i, vehicle in enumerate(journeys_data['body']):
-        route_id = vehicle['monitoredVehicleJourney']['lineRef']
-        if route_id not in feed.routes:
-            app.logger.debug("No route for: " + str(vehicle))
-            continue
-
         ent = msg.entity.add()
         ent.id = str(i)
+
+        route_id = vehicle['monitoredVehicleJourney']['lineRef']
         ent.trip_update.trip.route_id = route_id
 
-        route = feed.GetRoute(route_id)
+        date = vehicle['monitoredVehicleJourney']['framedVehicleJourneyRef']['dateFrameRef'].replace("-","")
+        ent.trip_update.trip.start_date = date
 
-        for trip in route.trips:
-            date = vehicle['monitoredVehicleJourney']['framedVehicleJourneyRef']['dateFrameRef'].replace("-","")
-            if not trip.service_period.IsActiveOn(date):
-                continue
-            start_time = vehicle['monitoredVehicleJourney']['originAimedDepartureTime']
-            start_time_seconds = int(start_time[:2])*60*60+int(start_time[2:4])*60
-            if trip.GetStartTime() != start_time_seconds:
-                continue
-            assert isinstance(trip, transitfeed.Trip)
-            stoptimes = trip.GetStopTimes()
-            if stoptimes[0].stop_id != vehicle['monitoredVehicleJourney']['originShortName']:
-                continue
-            if stoptimes[-1].stop_id != vehicle['monitoredVehicleJourney']['destinationShortName']:
-                continue
+        start_time = vehicle['monitoredVehicleJourney']['originAimedDepartureTime']
+        ent.trip_update.trip.start_time = start_time[:2]+":"+start_time[2:]+":00"
 
-            ent.trip_update.trip.trip_id = trip.trip_id
+        direction = vehicle['monitoredVehicleJourney']['directionRef']
+        ent.trip_update.trip.direction_id = int(direction)-1
 
-        if not ent.trip_update.trip.trip_id:
-            app.logger.debug("No trip for: " + str(vehicle))
 
         if 'onwardCalls' not in vehicle['monitoredVehicleJourney']:
             continue
 
         for call in vehicle['monitoredVehicleJourney']['onwardCalls']:
             stoptime = ent.trip_update.stop_time_update.add()
-            stoptime.stop_id = call['stopPointRef'].rsplit('/',1)[1]
+            stoptime.stop_sequence = int(call['order'])
             arrival_time = (dateutil.parser.parse(call['expectedArrivalTime']) - EPOCH).total_seconds()
             stoptime.arrival.time = int(arrival_time)
             departure_time = (dateutil.parser.parse(call['expectedDepartureTime']) - EPOCH).total_seconds()
